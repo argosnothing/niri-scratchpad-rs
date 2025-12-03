e68559c1d52ef5697faa878240c727eac9de4129
salivala <argosnothing@gmail.com>
2025-12-02 22:23:16 -0500

    focus on scratchpad is no longer needed to remove

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,18 @@ fn main() -> std::io::Result<()> {
     let state_file = State::new();
     let mut socket = Socket::connect()?;
 
-    let Ok(Response::FocusedOutput(focused_output)) = socket.send(Request::FocusedOutput)? else {
-        return Ok(());
-    };
     let Ok(Response::FocusedWindow(focused_window)) = socket.send(Request::FocusedWindow)? else {
         return Ok(());
     };
-    let Ok(Response::Windows(windows)) = socket.send(Request::Windows)? else {
+    let Ok(Response::Workspaces(workspaces)) = socket.send(Request::Workspaces)? else {
         return Ok(());
     };
 
     if let Some(scratchpad_number) = args.next().and_then(|s| s.parse::<i32>().ok()) {
+        let Some(current_workspace) = workspaces.iter().find(|workspace| workspace.is_focused)
+        else {
+            return Ok(());
+        };
         match state_file {
             Ok(mut state) => match focused_window {
                 Some(window) => match state.add_scratchpad(scratchpad_number, window.id, None) {
@@ -28,11 +29,19 @@ fn main() -> std::io::Result<()> {
                         state.update()?;
                     }
                     AlreadyExists(scratchpad) => {
-                        if window.id == scratchpad.id {
-                            ipc::stash(&mut socket, &state)?;
-                        } else {
-                            ipc::summon(&mut socket, &scratchpad)?;
-                        }
+                        if let Ok(Response::Windows(windows)) = socket.send(Request::Windows)? {
+                            if let Some(scratchpad_window) =
+                                windows.iter().find(|window| window.id == scratchpad.id)
+                            {
+                                if let Some(workspace_id) = scratchpad_window.workspace_id {
+                                    if workspace_id == current_workspace.id {
+                                        ipc::stash(&mut socket, &state)?;
+                                    } else {
+                                        ipc::summon(&mut socket, &scratchpad)?;
+                                    }
+                                };
+                            };
+                        };
                     }
                 },
                 None => {
